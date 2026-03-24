485841ce26aa36998e8b0e41cb13bfb8afff11c8
linbaumxiv <linbaum14@gmail.com>
2026-03-24 14:18:28 -0600

    docs: finalized sphinx build and module indexing

--- docs/conf.py
+++ docs/conf.py
@@ -10,7 +10,7 @@ import sys
 import django 
 
 sys.path.insert(0, os.path.abspath('..'))
-os.environ['DJANGO_SETTINGS-MODULE'] = 'eclectic_news.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'eclectic_news.settings'
 django.setup()
 
 project = 'News'
